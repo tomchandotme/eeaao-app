98c8866f8f1034c3abc7bcda7eae701dca6f521f
tomchan <tomchan@instelar.com>
2022-05-17 13:33:48 +0800

    code clean

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,45 @@ import 'styles/globals.scss';
 import { AppProps } from 'next/app';
 
 import '98.css';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>eeaao</title>
+                <meta name="description" content="eeaao" />
+                <meta name="theme-color" content="#002" />
+
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                />
+
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest" />
+            </Head>
+            <Component {...pageProps} />;
+        </>
+    );
 };
 
 export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,36 +5,11 @@ import cx from 'classnames';
 
 import Styles from 'styles/Home.module.scss';
 import UniversesMap from 'components/UniversesMap';
-import Head from 'next/head';
 
 const HomePage: NextPage = () => {
     const [selectedUniverse, setSelectedUniverse] = useState<string>();
     return (
         <div className={Styles.main}>
-            <Head>
-                <title>eeaao</title>
-                <meta name="description" content="eeaao" />
-                <meta name="theme-color" content="#002" />
-
-                <link
-                    rel="apple-touch-icon"
-                    sizes="180x180"
-                    href="/apple-touch-icon.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/favicon-16x16.png"
-                />
-                <link rel="manifest" href="/site.webmanifest" />
-            </Head>
             <div className="window">
                 <div className="title-bar">
                     <div className="title-bar-text">
